1f5e7f88296b39265ca98a0b6b9880e9baef3739
mashimine <mauricio@caylent.com>
2020-04-26 14:29:15 -0300

    add scripts for gettings credentials

---
+++ bin/x1c7/remove_sound.sh
@@ -0,0 +1,7 @@
+rm -rf /lib/firmware/intel/sof/
+rm -rf /lib/firmware/intel/sof-tplg/
+rm /etc/modprobe.d/alsa-base.conf 
+rm /etc/modprobe.d/blacklist.conf  
+rm /usr/share/alsa/ucm/sof-skl_hda_card/HiFi.conf
+rm /usr/share/alsa/ucm/sof-skl_hda_card
+rm /usr/share/alsa/ucm/sof-skl_hda_card/sof-skl_hda_card.conf 
